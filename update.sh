917e01d166e108b8ec04b23a992a6e023f5f598d
Patrick Miller <pamiller.pdx@gmail.com>
2024-10-10 00:58:36 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+bun upgrade
+deno upgrade rc
+fnm install 20
+fnm default 20
+fnm use 20
+npm update -g --latest
+corepack install --global pnpm
+yt-dlp -U
+curl https://raw.githubusercontent.com/patdx/dotfiles/main/install-gcm-linux.ts | bun run -
