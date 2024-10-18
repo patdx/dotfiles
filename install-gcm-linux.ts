e83663c3f218cc671a366b093446824daecab248
Patrick Miller <pamiller.pdx@gmail.com>
2024-10-19 02:34:01 +0900

    Check GCM version before updating

--- install-gcm-linux.ts
+++ install-gcm-linux.ts
@@ -3,8 +3,11 @@
 // https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/install.md
 
 import { $ } from 'bun';
+import fs from 'node:fs';
 
 type Release = {
+  /** @example "v2.6.0" */
+  tag_name?: string;
   assets: {
     name: string;
     browser_download_url: string;
@@ -15,6 +18,16 @@ export async function installGcmLinux() {
   const result: Release = await fetch(
     'https://api.github.com/repos/git-ecosystem/git-credential-manager/releases/latest',
   ).then((r) => r.json());
+
+  const latestVersion = getLatestVersion(result);
+  const installedVersion = await getInstalledVersion();
+
+  console.log(`Installed: ${installedVersion} Latest: ${latestVersion}`);
+
+  if (installedVersion === latestVersion) {
+    console.log('Already up to date');
+    return;
+  }
 
   const asset = result.assets.find(
     (a) =>
@@ -32,9 +45,9 @@ export async function installGcmLinux() {
     process.exit(1);
   }
 
-  console.log(`Downloading: ${asset.name}`);
+  using tempDir = new TempDir('.temp');
 
-  await $`curl -L -o gcm.tar.gz ${asset.browser_download_url}`;
+  await downloadFile(asset.browser_download_url, '.temp/gcm.tar.gz');
 
   const uid = process.getuid?.();
   console.log(`UID: ${uid}`);
@@ -61,6 +74,48 @@ export async function installGcmLinux() {
   console.log(`git config --global credential.credentialStore secretservice`);
 }
 
+class TempDir implements Disposable {
+  #path: string;
+
+  constructor(path: string) {
+    this.#path = path;
+    // clear it in case it already exists
+    fs.rmSync(this.#path, { recursive: true, force: true });
+    fs.mkdirSync(path, { recursive: true });
+  }
+
+  get path() {
+    return this.#path;
+  }
+
+  [Symbol.dispose]() {
+    // Remove the directory and its contents recursively
+    fs.rmSync(this.#path, { recursive: true, force: true });
+  }
+}
+
 if (import.meta.main) {
   await installGcmLinux();
+}
+
+async function downloadFile(url: string, filename: string) {
+  console.log(`Downloading file from ${url} to ${filename}`);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to download file: ${response.statusText}`);
+  }
+  await Bun.write(filename, response);
+  console.log(`Downloaded file to ${filename}`);
+}
+
+function getLatestVersion(release: Release) {
+  return release.tag_name?.replace(/^v/, '');
+}
+
+async function getInstalledVersion() {
+  const result = await $`git-credential-manager --version`;
+  // May return something like:
+  // 2.6.0+3c28096588f549cb46f36b552390514356830abe
+  const [version] = result.text().trim().split('+');
+  return version;
 }
