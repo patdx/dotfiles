0f45570a4b3a345c63b868fd700b61e3cc73f8b0
Patrick Miller <pamiller.pdx@gmail.com>
2024-10-19 02:13:15 +0900

    Add http import

--- install-gcm-linux.ts
+++ install-gcm-linux.ts
@@ -11,50 +11,56 @@ type Release = {
   }[];
 };
 
-const result: Release = await fetch(
-  'https://api.github.com/repos/git-ecosystem/git-credential-manager/releases/latest',
-).then((r) => r.json());
+export async function installGcmLinux() {
+  const result: Release = await fetch(
+    'https://api.github.com/repos/git-ecosystem/git-credential-manager/releases/latest',
+  ).then((r) => r.json());
 
-const asset = result.assets.find(
-  (a) =>
-    a.name.startsWith('gcm-linux_amd64') &&
-    !a.name.endsWith('symbols.tar.gz') &&
-    a.name.endsWith('.tar.gz'),
-);
-
-if (!asset) {
-  console.log(
-    `No asset found. All assets: ${JSON.stringify(
-      result.assets.map((asset) => asset.name),
-    )}`,
+  const asset = result.assets.find(
+    (a) =>
+      a.name.startsWith('gcm-linux_amd64') &&
+      !a.name.endsWith('symbols.tar.gz') &&
+      a.name.endsWith('.tar.gz'),
   );
-  process.exit(1);
+
+  if (!asset) {
+    console.log(
+      `No asset found. All assets: ${JSON.stringify(
+        result.assets.map((asset) => asset.name),
+      )}`,
+    );
+    process.exit(1);
+  }
+
+  console.log(`Downloading: ${asset.name}`);
+
+  await $`curl -L -o gcm.tar.gz ${asset.browser_download_url}`;
+
+  const uid = process.getuid?.();
+  console.log(`UID: ${uid}`);
+  if (uid !== 0) {
+    console.log('Requesting root privileges');
+    await $`sudo tar -xvf gcm.tar.gz -C /usr/local/bin`;
+  } else {
+    await $`tar -xvf gcm.tar.gz -C /usr/local/bin`;
+  }
+
+  await $`git-credential-manager configure`;
+
+  //
+
+  console.log('');
+  console.log(`Note: Additional configuration required for Linux`);
+  console.log(
+    'https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/credstores.md',
+  );
+  console.log(
+    'For GUI Linux users, it is recommended to use freedesktop.org Secret Service API:',
+  );
+  console.log('');
+  console.log(`git config --global credential.credentialStore secretservice`);
 }
 
-console.log(`Downloading: ${asset.name}`);
-
-await $`curl -L -o gcm.tar.gz ${asset.browser_download_url}`;
-
-const uid = process.getuid?.();
-console.log(`UID: ${uid}`);
-if (uid !== 0) {
-  console.log('Requesting root privileges');
-  await $`sudo tar -xvf gcm.tar.gz -C /usr/local/bin`;
-} else {
-  await $`tar -xvf gcm.tar.gz -C /usr/local/bin`;
+if (import.meta.main) {
+  await installGcmLinux();
 }
-
-await $`git-credential-manager configure`;
-
-//
-
-console.log('');
-console.log(`Note: Additional configuration required for Linux`);
-console.log(
-  'https://github.com/git-ecosystem/git-credential-manager/blob/release/docs/credstores.md',
-);
-console.log(
-  'For GUI Linux users, it is recommended to use freedesktop.org Secret Service API:',
-);
-console.log('');
-console.log(`git config --global credential.credentialStore secretservice`);
--- update.ts
+++ update.ts
@@ -5,7 +5,10 @@
 // 2. Run the following line in your terminal:
 //    curl https://raw.githubusercontent.com/patdx/dotfiles/main/update.ts | bun run -
 
+import type { OnLoadResult } from 'bun';
 import { $ } from 'bun';
+
+await init();
 
 await $`bun upgrade`;
 await $`deno upgrade`;
@@ -15,6 +18,42 @@ await $`fnm use 20`;
 await $`npm update -g --latest`;
 await $`corepack install --global pnpm`;
 await $`yt-dlp -U`;
-await $`curl https://raw.githubusercontent.com/patdx/dotfiles/main/install-gcm-linux.ts | bun run -`;
+await import(
+  'https://raw.githubusercontent.com/patdx/dotfiles/main/install-gcm-linux.ts'
+).then((m) => m.installGcmLinux());
 
 console.log('Update completed successfully!');
+
+async function init() {
+  const rx_any = /./;
+  const rx_http = /^https?:\/\//;
+  const rx_relative_path = /^\.\.?\//;
+
+  await Bun.plugin({
+    name: 'http_imports',
+    setup(build) {
+      async function load_http_module(href: string): Promise<OnLoadResult> {
+        const response = await fetch(href);
+        const text = await response.text();
+        if (response.ok) {
+          console.log('loaded ' + text);
+          return { contents: text, loader: 'tsx' };
+        } else {
+          throw new Error(`Failed to load module '${href}': ${text}`);
+        }
+      }
+
+      build.onResolve({ filter: rx_relative_path }, function (args) {
+        if (rx_http.test(args.importer)) {
+          return { path: new URL(args.path, args.importer).href };
+        }
+      });
+      build.onLoad({ filter: rx_any, namespace: 'http' }, function (args) {
+        return load_http_module('http:' + args.path);
+      });
+      build.onLoad({ filter: rx_any, namespace: 'https' }, function (args) {
+        return load_http_module('https:' + args.path);
+      });
+    },
+  });
+}
